d290db02012c118c23358cb36d4d44d28a58c564
Alberto Ruiz <alberto.ruiz@salesianoslosboscos.com>
2026-06-11 23:10:27 +0200

    vulnerabilidades

--- sql_migrate_estado_ticket.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE "BDTicket" ADD COLUMN IF NOT EXISTS "estado" TEXT;
-
-UPDATE "BDTicket" SET "estado" = 'confirmado' WHERE "confirmado" = true;
-UPDATE "BDTicket" SET "estado" = 'por confirmar' WHERE ("confirmado" = false OR "confirmado" IS NULL);
-
-ALTER TABLE "BDTicket" DROP COLUMN IF EXISTS "confirmado";
